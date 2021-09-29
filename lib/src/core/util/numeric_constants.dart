52eeb6c97123b9f3c49047c6130b17a9b3f4d443
juliana.dimeglio <juliana.dimeglio@globant.com>
2021-09-29 15:54:01 -0300

    Implementation of the Splash Screen

---
+++ lib/src/core/util/numeric_constants.dart
@@ -0,0 +1,8 @@
+abstract class NumericConstants {
+  static const double splashScreenGifMargin = 20;
+  static const int splashScreenGifDuration = 2500;
+  static const double splashScreenPadding = 150;
+  static const double tweenEnd = 40.0;
+  static const double tweenBegin = 0;
+  static const int splashTitleDuration = 1;
+}
---
+++ lib/src/core/util/string_constants.dart
@@ -0,0 +1,7 @@
+abstract class StringConstants {
+  static const String splashScreenBackground =
+      'assets/background-poke_ball.jpg';
+  static const String splashScreenGif = 'assets/pikachu-running.gif';
+  static const String splashScreenText = 'My Pokemon App';
+  static const String homePageTitle = 'Flutter Demo Home Page';
+}
